4de198b4162cea3e84e52043152fa342e88df2aa
Benjamin Grand <17108974+bgrand-ch@users.noreply.github.com>
2024-02-07 14:23:58 +0100

    fix: check element exists

--- src/utils/device.ts
+++
@@ -1,6 +0,0 @@
-export function isTouchDevice () {
-  const hasTouchEvent = 'ontouchstart' in window
-  const hasTouchPoints = navigator.maxTouchPoints > 0
-
-  return hasTouchEvent || hasTouchPoints
-}
--- src/utils/mouse.ts
+++ src/utils/mouse.ts
@@ -5,9 +5,17 @@ import type { MouseListener } from '../types'
 
 const defaultDistance = 24
 
+function hasAvailableElement (element?: HTMLElement) {
+  return typeof element !== 'undefined' && element.isConnected
+}
+
 export function getMouseListener (element: HTMLElement, distance = defaultDistance) {
   return (event: MouseEvent) => {
     try {
+      if (!hasAvailableElement(element)) {
+        throw new Error('No element')
+      }
+
       const { clientY, clientX } = event
       const documentElement = document.documentElement
       const documentOffset = documentElement.getBoundingClientRect()
@@ -26,35 +34,55 @@ export function getMouseListener (element: HTMLElement, distance = defaultDistan
 }
 
 export function showGrabbedInfo (element: HTMLElement, mouseListener?: MouseListener) {
-  const currentStyle = window.getComputedStyle(element)
+  try {
+    if (!hasAvailableElement(element)) {
+      throw new Error('No element')
+    }
 
-  if (currentStyle.display === 'none') {
-    element.style.display = 'block'
+    const currentStyle = window.getComputedStyle(element)
+
+    if (currentStyle.display === 'none') {
+      element.style.display = 'block'
+    }
+
+    if (currentStyle.position !== 'fixed') {
+      element.style.position = 'fixed'
+    }
+
+    if (['0', 'auto'].includes(currentStyle.zIndex)) {
+      element.style.zIndex = '99999'
+    }
+  } catch (err) {
+    const { message } = err as Error
+    console.warn(`${logScope} showGrabbedInfo - ${message}`)
+  } finally {
+    if (!mouseListener) {
+      return
+    }
+  
+    window.addEventListener('mousemove', mouseListener, false)
   }
-
-  if (currentStyle.position !== 'fixed') {
-    element.style.position = 'fixed'
-  }
-
-  if (['0', 'auto'].includes(currentStyle.zIndex)) {
-    element.style.zIndex = '99999'
-  }
-
-  if (!mouseListener) {
-    return
-  }
-
-  window.addEventListener('mousemove', mouseListener, false)
 }
 
 export function hideGrabbedInfo (element: HTMLElement, mouseListener?: MouseListener) {
-  element.style.display = 'none'
+  try {
+    if (!hasAvailableElement(element)) {
+      throw new Error('No element')
+    }
 
-  if (!mouseListener) {
-    return
+    if (element.style.display !== 'none') {
+      element.style.display = 'none'
+    }
+  } catch (err) {
+    const { message } = err as Error
+    console.warn(`${logScope} hideGrabbedInfo - ${message}`)
+  } finally {
+    if (!mouseListener) {
+      return
+    }
+
+    window.removeEventListener('mousemove', mouseListener, false)
   }
-
-  window.removeEventListener('mousemove', mouseListener, false)
 }
 
 export function initGrabbedInfo (editor: Editor, element: HTMLElement) {
